e677f1ab54f889176edd73d5055a80fc650a9a46
miozeng <544408511@qq.com>
2019-10-17 11:16:33 +0800

    edit

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,13 +5,13 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    ctx.body = 'hiegg';
   }
 
   async info() {
     const ctx = this.ctx;
-  //  const userId = ctx.params.id;
-    const userId =1
+    //  const userId = ctx.params.id;
+    const userId = 1
     const user = await ctx.service.user.find(userId);
     ctx.body = user;
   }
